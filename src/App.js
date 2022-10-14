641abf15afc36ca5437fe0d2ca53152adc43975d
Chirag Chhajed <chhajedchirag18@gmail.com>
2022-10-14 18:58:01 +0530

    change made

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Components/Header';
+import './App.css'
+
+
+function App() {
+  return (
+    <>
+    <Header/>
+    </>
+  );
+}
+
+export default App;
